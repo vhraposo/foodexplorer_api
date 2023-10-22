80d1aa00efd8905858783d8a4495c40ed9a655aa
vhraposo <thevhup@gmail.com>
2023-10-22 00:35:16 -0300

    feat: add put - update method at users controller

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -19,6 +19,30 @@ class UsersController {
     await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, hashedPassword])
     return response.status(201).json() 
   }
+
+  async update(request, response){
+    const { name, email, password } = request.body
+    const { id } = request.params 
+
+    const database = await sqliteConnection()
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+    if(!user){
+      throw new AppError('User not found!')
+    }
+
+    const userWithUpdatedEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+      throw new AppError('Email address already used.')
+    }
+
+    user.name = name
+    user.email = email
+
+    await database.run(`UPDATE users SET name = ?, email = ?, password = ?, updated_at = ? WHERE id = ?`, [user.name, user.email, user.password, new Date(), id])
+    return response.json()
+  }
 }
 
 module.exports =  UsersController
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,6 +6,8 @@ const UsersController = require('../controllers/UsersController')
 const usersController = new UsersController()
 
 usersRoutes.post('/', usersController.create)
+usersRoutes.put('/:id', usersController.update)
+
 
 module.exports = usersRoutes
 
