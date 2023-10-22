e7f76546f38556e76deb41e2d1499fa2094ee6bf
vhraposo <thevhup@gmail.com>
2023-10-21 23:35:29 -0300

    refactor: add new table field to createUsers

--- src/database/knex/migrations/20231022015225_createUsers.js
+++ src/database/knex/migrations/20231022015225_createUsers.js
@@ -4,7 +4,8 @@ exports.up = knex => knex.schema.createTable('users', table => {
     table.text('name')
     table.text('email')
     table.text('password')
-
+    table.boolean('isAdmin').defaultTo(false);
+    table.string('avatar').defaultTo('https://res.cloudinary.com/demo/image/upload/w_100,h_100,c_thumb,g_face,r_20,d_avatar.png/non_existing_id.png')
     table.timestamp('created_at').defaultTo(knex.fn.now())
     table.timestamp('updated_at').defaultTo(knex.fn.now())
 
