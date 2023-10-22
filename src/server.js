80baaa6e3f668abf1eeb7cdd6cf148dd7feb6665
vhraposo <thevhup@gmail.com>
2023-10-21 21:32:37 -0300

    feat: add error exception handler at userscontroller

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require('express-async-errors')
+
+const AppError = require('./utils/AppError')
 const express = require('express')
 const routes = require('./routes')
 
@@ -5,6 +8,20 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+app.use(( error, request, response, next ) => {
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status: 'error',
+            message: error.message
+        })
+    }
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error'
+    })
+})
 
 const PORT = 3334
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
