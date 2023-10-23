8941592370d2971bab1dc02b49825fa0430c96ed
vhraposo <thevhup@gmail.com>
2023-10-23 19:10:40 -0300

    feat: create dishes routes

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -7,8 +7,7 @@ const dishesController = new DishesController()
 
 dishesRoutes.post('/:user_id', dishesController.create)
 dishesRoutes.get('/:id', dishesController.show)
-
-
+dishesRoutes.delete('/:id', dishesController.delete)
 
 
 module.exports = dishesRoutes
