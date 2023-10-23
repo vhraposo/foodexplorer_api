f17df7d179cdb2937454b44b2afbd305aa5bf118
vhraposo <thevhup@gmail.com>
2023-10-23 19:11:24 -0300

    fix: added error handling if dish doesn't exist at dishes controller

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -2,7 +2,7 @@ const knex = require('../database/knex');
 
 class DishesController {
   async create(request, response) {
-    const { name, description, price, image, category, ingredients } = request.body;
+    const { name, description, price, image, category, ingredients } = request.body
 
     try {
 
@@ -12,35 +12,51 @@ class DishesController {
         image,
         price,
         category,
-      });
+      })
 
-     
-      const ingredientsInsert = ingredients.map((ingredientName) => {
+      const ingredientsInsert = ingredients.map((name) => {
         return {
-          dish_id: dish_id,
-          name: ingredientName,
-        };
-      });
+          dish_id,
+          name,
+        }
+      })
 
-      await knex('ingredients').insert(ingredientsInsert);
+      await knex('ingredients').insert(ingredientsInsert)
 
-      response.status(201).json({ dish_id });
+      response.status(201).json({ dish_id })
+
     } catch (error) {
       console.error(error);
-      response.status(500).json({ error: 'Erro interno do servidor' });
+      response.status(500).json({ error: 'Internal server error' })
     }
   }
 
   async show(request, response){
     const { id } = request.params
-
     const dish = await knex('dishes').where({ id }).first()
+
+    if(!dish){
+      return response.status(404).json({error: 'Dish not found'})
+    }
+
     const ingredients = await knex('ingredients').where({ dish_id: id}).orderBy('name')
+
+    if (ingredients.length === 0) {
+      return response.status(404).json({ error: 'Dish has no associated ingredients' });
+    }
 
     return response.json({
         ...dish,
         ingredients
     })
+  }
+
+  async delete(request, response){
+    const { id } = request.params
+    
+    await knex('dishes').where({ id }).delete()
+
+    return response.json()
   }
 }
 
