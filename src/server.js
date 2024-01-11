52ad906bb66647c5050db9446eaa87d1bd170665
vhraposo <thevhup@gmail.com>
2024-01-11 20:32:47 -0300

    feat: add body-parser middleware to handle larger request payloads

--- src/server.js
+++ src/server.js
@@ -5,10 +5,11 @@ const AppError = require('./utils/AppError')
 const express = require('express')
 const routes = require('./routes')
 const cors = require('cors')
-
+const bodyParser = require('body-parser')
 const app = express()
 
-
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 app.use(express.json())
 app.use(cors('*'))
 app.use(routes)
