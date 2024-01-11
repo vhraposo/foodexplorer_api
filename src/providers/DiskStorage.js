81a2775198cd1b5b535accd5e19c989a83c4b72b
vhraposo <thevhup@gmail.com>
2024-01-11 20:33:12 -0300

    fix: typo in variable name

--- src/providers/DiskStorage.js
+++ src/providers/DiskStorage.js
@@ -12,7 +12,7 @@ class DiskStorage {
         return file
     }
     async deleteFile(file){
-        const filePathh = path.resolve(uploadConfig.UPLOADS_FOLDER, file)
+        const filePath = path.resolve(uploadConfig.UPLOADS_FOLDER, file)
         try{
             await fs.promises.stat(filePath)
         }catch{
